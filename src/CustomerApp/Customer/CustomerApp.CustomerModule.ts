84171a2c75b12752a60b0bf947ebee729284ec64
KumariAnnpurna <annpurnaraisharma@gmail.com>
2021-12-28 15:56:17 +0530

    4 files modified, 4 added

--- src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
+++ src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
@@ -1,21 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {FormsModule , ReactiveFormsModule} from "@angular/forms"
 import {RouterModule} from "@angular/router"
 import { CustomerComponent } from './CustomerApp.component';
+import {GridComponent} from "../Utility/CustomerApp.GridComponent"
 import { CustomerRoutes } from '../Routing/CustomerApp.CustomerRouting';
+import {HttpModule} from "@angular/http"
+import {HttpClientModule,HTTP_INTERCEPTORS} from "@angular/common/http"
+import { MyInterceptor } from '../Utility/Utility.HttpInterceptor';
+
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
-     CustomerComponent
+     CustomerComponent ,GridComponent
   ],
   imports: [
-   CommonModule,
-    FormsModule,//simple bracket []or {}or ngmodule
-    ReactiveFormsModule,//validation,formcontrol,form group
-    RouterModule.forChild( CustomerRoutes )
-   
+    RouterModule.forChild(CustomerRoutes),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule ,
+    HttpClientModule
   ],
-  providers: [],
+  exports: [
+    GridComponent
+  ],
+  providers: [
+   {provide: HTTP_INTERCEPTORS, useClass: MyInterceptor , 
+  multi:true}
+
+  ],
   bootstrap: [CustomerComponent]
 })
 export class CustomerModule { }
